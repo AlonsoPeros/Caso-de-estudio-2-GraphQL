dd76b83cecd8c22fc8bb5d0d51c29c70e1c250e5
AlonsoPeros <kevin.perez03@uptc.edu.co>
2024-11-09 15:32:44 -0500

    INICIO

---
+++ src/main/java/com/uptc/frw/casodeestudiographql/CasoDeEstudioGraphQlApplication.java
@@ -0,0 +1,13 @@
+package com.uptc.frw.casodeestudiographql;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CasoDeEstudioGraphQlApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CasoDeEstudioGraphQlApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/uptc/frw/casodeestudiographql/CasoDeEstudioGraphQlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uptc.frw.casodeestudiographql;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CasoDeEstudioGraphQlApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
