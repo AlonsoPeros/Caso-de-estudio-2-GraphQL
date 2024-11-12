620b882d82d2e3052446b4272283645042809e58
yacuervo <yamithcuervo@gmail.com>
2024-11-11 19:28:46 -0500

    crear servicio de cuartel

---
+++ src/main/java/com/uptc/frw/casodeestudiographql/jpa/repository/CuartelRepository.java
@@ -0,0 +1,7 @@
+package com.uptc.frw.casodeestudiographql.jpa.repository;
+
+import com.uptc.frw.casodeestudiographql.jpa.model.Cuartel;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CuartelRepository extends JpaRepository<Cuartel, Long> {
+}
---
+++ src/main/java/com/uptc/frw/casodeestudiographql/service/CuartelService.java
@@ -0,0 +1,14 @@
+package com.uptc.frw.casodeestudiographql.service;
+
+
+import com.uptc.frw.casodeestudiographql.jpa.repository.CuartelRepository;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CuartelService {
+
+    CuartelRepository cuartelRepository;
+
+
+
+}
